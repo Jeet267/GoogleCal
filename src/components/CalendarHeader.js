c6b853c94c95d734f040f6f34ba411fbdf5921e6
Jeet267 <152002098+Jeet267@users.noreply.github.com>
2024-09-25 00:27:01 +0530

    a

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -1,30 +1,59 @@
 import dayjs from "dayjs";
 import React, { useContext } from "react";
+
+
 import logo from "../assets/logo.png";
 import GlobalContext from "../context/GlobalContext";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-export default function CalendarHeader() {
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { useState } from "react";
+import HelpModal from "./HelpModal";
+import SettingsModal from "./settingmodal";
+export default function CalendarHeader({ setViewState }) {
   const { monthIndex, setMonthIndex } = useContext(GlobalContext);
+  const [showSettingsModal, setShowSettingsModal] = useState(false);
+  const [showHelpModal, setShowHelpModal] = useState(false);
+  const [viewType, setViewType] = useState("Month");
+
   function handlePrevMonth() {
     setMonthIndex(monthIndex - 1);
   }
   function handleNextMonth() {
     setMonthIndex(monthIndex + 1);
   }
+  function handleViewChange(e) {
+    setViewType(e.target.value);
+    setViewState(e.target.value);
+  }
+
   function handleReset() {
     setMonthIndex(
       monthIndex === dayjs().month()
         ? monthIndex + Math.random()
         : dayjs().month()
     );
+    
   }
   return (
     <header className="px-4 py-2 flex flex-col sm:flex-row sm:items-center sm:justify-between">
       <div className="flex flex-col sm:flex-row items-center sm:space-x-5 space-y-3 sm:space-y-0">
-        <img src={logo} alt="calendar" className="w-10 h-10 sm:w-12 sm:h-12" />
-        <h1 className="text-lg sm:text-xl text-gray-500 font-bold">Calendar</h1>
+        <img
+          src={logo}
+          alt="calendar"
+          className="w-10 h-10 sm:w-12 sm:h-12"
+        />
+        <h1 className="text-lg sm:text-xl text-gray-500 font-bold">
+          Calendar
+        </h1>
         <div className="flex space-x-2">
-          <button onClick={handleReset} className="border rounded py-1 px-3 sm:py-2 sm:px-4 text-sm sm:text-base">
+          <button
+            onClick={handleReset}
+            className="border rounded py-1 px-3 sm:py-2 sm:px-4 text-sm sm:text-base"
+          >
             Today
           </button>
           <button onClick={handlePrevMonth}>
@@ -39,7 +68,9 @@ export default function CalendarHeader() {
           </button>
         </div>
         <h2 className="text-lg sm:text-xl text-gray-500 font-bold">
-          {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
+          {dayjs(new Date(dayjs().year(), monthIndex)).format(
+            "MMMM YYYY"
+          )}
         </h2>
       </div>
       <div className="mt-3 sm:mt-0">
@@ -54,15 +85,28 @@ export default function CalendarHeader() {
           <UserButton />
         </SignedIn>
       </div>
+      <span
+        className="material-icons-outlined cursor-pointer text-white-600 mx-2"
+        onClick={() => setShowSettingsModal(true)} // Open settings modal
+      >
+        settings
+      </span>
+
+      <select
+        value={viewType}
+        onChange={handleViewChange}
+        className="select-custom"
+      >
+        <option value="Day">Day</option>
+        <option value="Week">Week</option>
+        <option value="Month">Month</option>
+      </select>
+      {showSettingsModal && (
+        <SettingsModal onClose={() => setShowSettingsModal(false)} />
+      )}
+      {showHelpModal && (
+        <HelpModal onClose={() => setShowHelpModal(false)} />
+      )}
     </header>
   );
 }
-
-
-
-
-
-
-
-
-
