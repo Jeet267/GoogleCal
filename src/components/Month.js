d923fb897574ff7a9fd33ec3896ec6693f7c2fb0
Jeet267 <152002098+Jeet267@users.noreply.github.com>
2024-09-24 19:55:31 +0530

    done

--- src/components/Month.js
+++ src/components/Month.js
@@ -2,7 +2,7 @@ import React from "react";
 import Day from "./Day";
 export default function Month({ month }) {
   return (
-    <div className="hidden md:flex-1 md:grid md:grid-cols-7 md:grid-rows-5 ">
+    <div className="hidden md:flex-1 md:grid md:grid-cols-7 md:grid-rows-5 h-screen ">
       {month.map((row, i) => (
         <React.Fragment key={i}>
           {row.map((day, idx) => (
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,7 @@ import SmallCalendar from "./SmallCalendar";
 import Labels from "./Labels";
 export default function Sidebar() {
   return (
-    <aside className="border p-5 w-full md:w-64 lg:w-64 m-auto mx-auto">
+    <aside className="border p-5 w-full md:w-64 lg:w-64  mx-auto mt-0">
       <CreateEventButton />
       <SmallCalendar />
       <Labels />
